6aecf8d496881ffbe55a2acf8b8148d3ca5d1976
tgsse <tamas.gura.sse@gmail.com>
2023-10-11 18:43:53 +0200

    added common ui components

---
+++ app/src/main/java/com/ix/diary/ui/components/TopBar.kt
@@ -0,0 +1,47 @@
+package com.ix.diary.ui.components
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import com.ix.diary.ui.theme.DiaryTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    title: String,
+    actions: @Composable RowScope.() -> Unit = {},
+    navigationIcon: @Composable () -> Unit = {},
+) {
+    TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+            actionIconContentColor = MaterialTheme.colorScheme.onPrimary,
+            navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+        ),
+        title = {
+            Text(
+                text = title,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.displayMedium,
+            )
+        },
+        actions = actions,
+        navigationIcon = navigationIcon,
+    )
+}
+
+@Preview
+@Composable
+private fun TopBarPreview() {
+    DiaryTheme {
+        TopBar("title") {}
+    }
+}
